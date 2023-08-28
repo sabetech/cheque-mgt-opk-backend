9962b9df27876da3946f3a70218e1eed86c3bb6d
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2023-08-28 15:23:40 +0000

    [WIP] - User management

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -73,7 +73,7 @@ class UserController extends Controller
         $user->assignRole($request->input('role'));
 
         $user->save();
-
+        11                  
         return response()->json([
             'message' => 'User updated successfully',
             'data' => $user
